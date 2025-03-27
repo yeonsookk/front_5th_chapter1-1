d268b074a1c48e86274a4e16612f7f273a8f795d
KKys <oops9798@gmail.com>
2025-03-27 21:29:40 +0900

    Nav 현재 메뉴 강조

--- src/components/page/home/index.js
+++ src/components/page/home/index.js
@@ -4,7 +4,7 @@ import Footer from "../../common/Footer";
 const index = () => /* HTML */ `
   <div class="bg-gray-100 min-h-screen flex justify-center">
     <div class="max-w-md w-full">
-      ${Header()} ${Nav()}
+      ${Header()} ${Nav("/")}
 
       <main class="p-4">
         <div class="mb-4 bg-white rounded-lg shadow p-4">
