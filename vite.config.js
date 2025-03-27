7852f0443584ce297941d69cc177866d0d6b85c4
KKys <oops9798@gmail.com>
2025-03-27 18:30:09 +0900

    빌드 디렉토리 설정

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   base: "/front_5th_chapter1-1",
+  build: {
+    outDir: "docs",
+  },
   test: {
     globals: true,
     environment: "jsdom",
